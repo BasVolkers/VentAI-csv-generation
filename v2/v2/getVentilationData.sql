afa1b5544c0fae77078b663f3b216fff7f6b7750
BasVolkers <basvolkers99@gmail.com>
2023-07-14 17:06:25 +0200

    Remove mechvent from getVentilationData for now

--- v2/v2/getVentilationData.sql
+++ v2/v2/getVentilationData.sql
@@ -40,35 +40,35 @@ select ce.SUBJECT_ID, ce.HADM_ID, ce.ICUSTAY_ID, ce.CHARTTIME
       else null end
     ) as fio2_chartevents
 -- STEP 3: Get mechanical ventilation
-  , max(
-    case
-      when itemid is null or value is null then 0 -- can't have null values
-      when itemid = 720 and value != 'Other/Remarks' THEN 1  -- VentTypeRecorded
-      when itemid = 223848 and value != 'Other' THEN 1
-      when itemid = 223849 then 1 -- ventilator mode
-      when itemid = 467 and value = 'Ventilator' THEN 1 -- O2 delivery device == ventilator
-      when itemid in
-        (
-        445, 448, 449, 450, 1340, 1486, 1600, 224687 -- minute volume
-        , 639, 654, 681, 682, 683, 684,224685,224684,224686 -- tidal volume
-        , 218,436,535,444,459,224697,224695,224696,224746,224747 -- High/Low/Peak/Mean/Neg insp force ("RespPressure")
-        , 221,1,1211,1655,2000,226873,224738,224419,224750,227187 -- Insp pressure
-        , 543 -- PlateauPressure
-        , 5865,5866,224707,224709,224705,224706 -- APRV pressure
-        , 60,437,505,506,686,220339,224700 -- PEEP
-        , 3459 -- high pressure relief
-        , 501,502,503,224702 -- PCV
-        , 223,667,668,669,670,671,672 -- TCPCV
-        , 224701 -- PSVlevel
-        )
-        THEN 1
-      else 0
-    end
-    ) as MechVent,
-    avg(data_.PEEP) as PEEP, avg(data_.tidal_volume) as tidal_volume, avg(data_.plateau_pressure) as plateau_pressure
+  -- , max(
+  --   case
+  --     when itemid is null or value is null then 0 -- can't have null values
+  --     when itemid = 720 and value != 'Other/Remarks' THEN 1  -- VentTypeRecorded
+  --     when itemid = 223848 and value != 'Other' THEN 1
+  --     when itemid = 223849 then 1 -- ventilator mode
+  --     when itemid = 467 and value = 'Ventilator' THEN 1 -- O2 delivery device == ventilator
+  --     when itemid in
+  --       (
+  --       445, 448, 449, 450, 1340, 1486, 1600, 224687 -- minute volume
+  --       , 639, 654, 681, 682, 683, 684,224685,224684,224686 -- tidal volume
+  --       , 218,436,535,444,459,224697,224695,224696,224746,224747 -- High/Low/Peak/Mean/Neg insp force ("RespPressure")
+  --       , 221,1,1211,1655,2000,226873,224738,224419,224750,227187 -- Insp pressure
+  --       , 543 -- PlateauPressure
+  --       , 5865,5866,224707,224709,224705,224706 -- APRV pressure
+  --       , 60,437,505,506,686,220339,224700 -- PEEP
+  --       , 3459 -- high pressure relief
+  --       , 501,502,503,224702 -- PCV
+  --       , 223,667,668,669,670,671,672 -- TCPCV
+  --       , 224701 -- PSVlevel
+  --       )
+  --       THEN 1
+  --     else 0
+  --   end
+  --   ) as MechVent
+    ,avg(data_.PEEP) as PEEP, avg(data_.tidal_volume) as tidal_volume, avg(data_.plateau_pressure) as plateau_pressure
 
   from chartevents ce 
-  LEFT JOIN data_ ON (data_.icustay_id = ce.icustay_id) AND (data_.charttime = ce.charttime)
+  JOIN data_ ON (data_.icustay_id = ce.icustay_id) AND (data_.charttime = ce.charttime)
   where ce.value is not null and ce.error IS DISTINCT FROM 1
   group by ce.subject_id, ce.hadm_id, ce.icustay_id, ce.charttime
   ORDER BY ce.subject_id, ce.hadm_id, ce.icustay_id, ce.charttime
@@ -78,5 +78,5 @@ select base.*,
   plateau_pressure - peep as driving_pressure,
   base.tidal_volume / ibw.ideal_body_weight as adjusted_tidal_volume
 from base 
-JOIN getIdealBodyWeight ibw on (ibw.icustay_id = base.icustay_id)
-where mechvent = 1;
+JOIN getIdealBodyWeight ibw on (ibw.icustay_id = base.icustay_id);
+-- where mechvent = 1;
